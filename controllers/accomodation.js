4ab5eeb1d6d5f7da93459c6e6e6c6b5ae4a07414
Alex Brooks <alexbrooks96@hotmail.co.uk>
2018-03-28 22:27:23 +0100

    Started working on view individual property page

--- controllers/accomodation.js
+++ controllers/accomodation.js
@@ -23,23 +23,23 @@ module.exports = function(app){
 	});
 };
 
-	router.post('/addProperty', function(req, res){
-		var newProperty = new Accomodation(req.body);
-		newProperty.properties.addressL1 = req.body.addressL1;
-		newProperty.properties.addressL2 = req.body.addressL2;
-		newProperty.properties.city = req.body.city;
-		newProperty.properties.county = req.body.county;
-		newProperty.properties.postcode = req.body.postcode;
-		newProperty.geometry.coordinates = [req.body.longitude, req.body.latitude];
-		newProperty.save(function(err){
-			if (err) {
-				res.send(err);
-			}			
-			res.json(req.body);
-		});
-
-
+router.post('/addProperty', function(req, res){
+	var newProperty = new Accomodation(req.body);
+	newProperty.properties.addressL1 = req.body.addressL1;
+	newProperty.properties.addressL2 = req.body.addressL2;
+	newProperty.properties.city = req.body.city;
+	newProperty.properties.county = req.body.county;
+	newProperty.properties.postcode = req.body.postcode;
+	newProperty.geometry.coordinates = [req.body.longitude, req.body.latitude];
+	newProperty.save(function(err){
+		if (err) {
+			res.send(err);
+		}			
+		res.json(req.body);
 	});
+
+
+});
 
 router.get('/addproperty', isLoggedIn, function(req, res){
 	res.render('addproperty', {accomodation: req.accomodation, user: req.user});
@@ -53,6 +53,16 @@ router.get('/propertylist', isLoggedIn, function(req, res){
 	});
 });
 
+router.get('/viewproperty', isLoggedIn, function(req,res){
+
+	var propID = req.body.propID;
+	AccomodationModel.findOne({id: propID}, function(err, data){
+	console.log(propID);
+		res.render('viewproperty', {accomodation: data, user: req.user});
+		});
+	});
+
+
 module.exports = router;
 
 function isLoggedIn(req, res, next){
@@ -62,3 +72,4 @@ function isLoggedIn(req, res, next){
 
 	res.redirect('/login');
 };
+
